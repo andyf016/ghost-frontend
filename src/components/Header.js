6c2bbc615ab1cfed570237bb5afd55e5650b4261
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-12 00:23:43 -0500

    add mui start header start topbar

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+const Header = () => {
+    return (
+        <Typography className='App-header' variant="h4">
+           Anonymous Blather with Sentiment
+        </Typography>
+    )
+}
+
+export default Header
---
+++ src/components/TopBar.js
@@ -0,0 +1,15 @@
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/ToolBar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+
+const TopBar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default TopBar
