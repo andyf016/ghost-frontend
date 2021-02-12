09c0bca12b996b1c50eb4c8387056b1dd931072b
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-12 00:34:14 -0500

    update packages, progress on topbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import './App.css'
 import { useState, useEffect } from "react"
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Container from '@material-ui/core/Container'
-
-
 import Header from './components/Header'
+import TopBar from './components/TopBar'
+
 function App() {
   return (
     <Container maxWidth='xs'>
     <div className="container">
+      <TopBar></TopBar>
       <header className='App-header'>
       <Header />
       </header>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -2,13 +2,22 @@ import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/ToolBar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
-
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 
 const TopBar = () => {
     return (
-        <div>
-            
-        </div>
+        <AppBar>
+              <ToolBar>
+                <IconButton>
+                  <MenuIcon />
+                  <Typography variant="h6">
+                  Stuff goes here
+                  </Typography>
+                  <Button>Login</Button>
+                </IconButton>
+              </ToolBar>
+            </AppBar>
     )
 }
 
