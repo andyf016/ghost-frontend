10222cc5e665469aab9fb6f9f9ebeebbe7a1b557
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-13 12:23:57 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
+
 
 const Footer = () => {
     return (
-        <div>
-            
-        </div>
+        <footer>
+            <p>Copyright &copy; 2021</p>
+            <Link to='/about'>About</Link>
+        </footer>
     )
 }
 
