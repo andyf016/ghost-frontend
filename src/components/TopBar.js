32914a4faabe785c880099eacf52f007df64b888
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-24 03:39:33 -0500

    add menu to topBar

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,22 +1,46 @@
+import {useState } from 'react'
 import AppBar from '@material-ui/core/AppBar'
 import ToolBar from '@material-ui/core/ToolBar'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
 import "fontsource-roboto";
 
 const TopBar = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
     return (
         <AppBar>
               <ToolBar>
                 <IconButton>
-                  <MenuIcon />
+                  <MenuIcon onClick={handleClick} />
+                  <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
                   </IconButton>
                   <Typography variant="h6">
                   Stuff goes here
                   </Typography>
-                  <Button>Login</Button>
+                 
                 
               </ToolBar>
             </AppBar>
