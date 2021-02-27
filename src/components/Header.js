42b31435294b3624f4da3d713f0c188a54941219
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-27 12:59:03 -0500

    render post button depending on path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
+import { useLocation } from 'react-router-dom'
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Button from "@material-ui/core/Button"
 import "fontsource-roboto";
 
 const Header = ({onAdd, showAdd}) => {
+    const location = useLocation()
     return (
         <Container maxWidth='md'>
         <Typography variant="h4">
            Anonymous Blather with Sentiment
         </Typography>
         <br/>
-        <Button onClick={onAdd} color={showAdd ? 'primary' : 'secondary'}>{showAdd ? 'Close Form' : 'Post Reply'}</Button>
+        {location.pathname === '/' && <Button 
+            onClick={onAdd} 
+            color={showAdd ? 'primary' : 'secondary'}>
+            {showAdd ? 'Close Form' : 'Post Reply'}
+        </Button>}
         </Container>
     )
 }
