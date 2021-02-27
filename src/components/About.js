22bd8c7063280b05888069fc74484116393ea46e
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-27 13:03:08 -0500

    styling and adding typography to about and footer

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,13 @@
 import { Link } from 'react-router-dom'
+import Typography from "@material-ui/core/Typography";
+import "fontsource-roboto";
 
 const About = () => {
     return (
         <div>
-            <h4>Version 1.0.0</h4>
-            <Link to='/'>Back</Link>
+            <Typography variant='h4'>Version 1.0.0</Typography>
+            <Typography>
+            <Link to='/'>Back</Link></Typography>
         </div>
     )
 }
